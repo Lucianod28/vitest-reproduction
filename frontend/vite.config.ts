957b00829287374cd42fa5cafb6be6ac62505e5e
Luciano de la Iglesia <Lucianod28@gmail.com>
2022-12-08 22:26:01 -0800

    minimize reproduction

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,29 +5,9 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    host: true,
-    port: 3000,
-  },
   test: {
-    environment: 'jsdom',
-    globals: true,
     transformMode: {
       web: [/\.[jt]sx?$/],
     },
-    setupFiles: './setupVitest.ts',
-    coverage: {
-      include: ['src/'],
-      all: true,
-      lines: 95,
-      functions: 95,
-      branches: 95,
-      statements: 95
-    }
   },
-  resolve: {
-    conditions: ['development', 'browser'],
-  },
-
 });
